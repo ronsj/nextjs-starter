b8c004fe34245ccaf9f79a76c5a8362795bdf5f1
Ron San Jose <ronsj1@gmail.com>
2022-02-08 19:05:51 -0800

    add comment for eslint options

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
-    // Only run ESLint on the 'src' directory during production builds (next build)
+    /**
+     *  Only run ESLint on the 'src' directory during production builds (next build)
+     *  By default, Next.js will run ESLint for all files in the pages/, components/, and lib/ directories.
+     *  https://nextjs.org/docs/basic-features/eslint#linting-custom-directories-and-files
+     *
+     */
     dirs: ['src'],
   },
 }
